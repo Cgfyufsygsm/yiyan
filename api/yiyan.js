78c885c4aa61480a9afa773ecabd0605b33a5208
Cgfyufsygsm <i@imyangty.com>
2024-11-27 21:07:56 +0800

    fix cors

--- api/yiyan.js
+++ api/yiyan.js
@@ -7,6 +7,9 @@ const lyrics = JSON.parse(fs.readFileSync(path.join(__dirname, '../lyrics.json')
 module.exports = (req, res) => {
   const randomIndex = Math.floor(Math.random() * lyrics.length);
   const randomLyric = lyrics[randomIndex];
+  // 添加 CORS 头
+  res.setHeader('Access-Control-Allow-Origin', '*'); // 允许所有域名发起的请求
+  res.setHeader('Access-Control-Allow-Methods', 'GET'); // 允许的请求方法
 
   // 返回多个字段，包括歌词、歌曲名、艺术家
   res.status(200).json({
