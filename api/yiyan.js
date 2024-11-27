0ce9ffd0aed856a586fadeb678c040013f922df1
Cgfyufsygsm <i@imyangty.com>
2024-11-27 20:51:27 +0800

    first

---
+++ api/yiyan.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+// 读取歌词数据
+const lyrics = JSON.parse(fs.readFileSync(path.join(__dirname, '../lyrics.json')));
+
+module.exports = (req, res) => {
+  const randomIndex = Math.floor(Math.random() * lyrics.length);
+  const randomLyric = lyrics[randomIndex];
+
+  // 返回多个字段，包括歌词、歌曲名、艺术家
+  res.status(200).json({
+    lyric: randomLyric.lyric,
+    song: randomLyric.song,
+    artist: randomLyric.artist
+  });
+};
